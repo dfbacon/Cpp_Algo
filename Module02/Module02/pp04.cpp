cc7db6e19d910325642a02f737cc3819e6ba0ad2
Daniel Bacon <dbacon338@gmail.com>
2019-09-08 14:55:53 -0700

    Compiling and running, return value is off by 1.

--- Module02/Module02/pp04.cpp
+++ Module02/Module02/pp04.cpp
@@ -3,9 +3,11 @@
  Class: kSmall
  Assignment: Module 2 - Programming Problem #4.
  */
-#include <algorithm>
-using namespace std;
 
+#include <algorithm>
+#include <iostream> // REMOVE ME
+
+using namespace std;
 
 void sortThreeEntries(int anArray[], int first, int middle, int last) {
     if (anArray[first] > anArray[middle])
@@ -26,24 +28,24 @@ void sortThreeEntries(int anArray[], int first, int middle, int last) {
 int partition(int anArray[], int first, int last) {
     int mid, pivot, pivotIndex, leftIndex, rightIndex;
     bool finished = false;
-    
+
     mid = first + (last - first) / 2;
     sortThreeEntries(anArray, first, mid, last);
     swap(anArray[mid], anArray[last - 1]);
 
     pivotIndex = last - 1;
     pivot = anArray[pivotIndex];
-    
+
     leftIndex = first + 1;
     rightIndex = last - 2;
-    
+
     while (!finished) {
         while (anArray[leftIndex] < pivot)
             leftIndex += 1;
-        
+
         while (anArray[rightIndex] > pivot)
             rightIndex -= 1;
-        
+
         if (leftIndex < rightIndex) {
             //Move anArray[firstUnknown] into S1
             swap(anArray[leftIndex], anArray[rightIndex]);
@@ -53,10 +55,15 @@ int partition(int anArray[], int first, int last) {
             finished = true;
         }
     }
-    
+
     swap(anArray[pivotIndex], anArray[leftIndex]);
     pivotIndex = leftIndex;
     
+    cout << "\nTESTING: anArray is now: [";
+    for (int i = 0; i < 6; i++) {
+        cout << anArray[i] << ", ";
+    }
+    cout << "]" << endl;
     
     return pivotIndex;
 }
@@ -75,7 +82,7 @@ int kSmall(int k, int anArray[], int first, int last) {
         return kSmall(k, anArray, first, pivotIndex - 1);
 
     else if (k == pivotIndex - first + 1)
-        return pivotIndex;
+        return anArray[pivotIndex]; // returning incorrect value
 
     else
         return kSmall(k - (pivotIndex - first + 1), anArray, pivotIndex + 1, last);
