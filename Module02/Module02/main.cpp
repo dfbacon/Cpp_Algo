5ddfc79c327c89f7bddc3020f6fa0701810d655a
Daniel Bacon <dbacon338@gmail.com>
2019-09-08 14:29:02 -0700

    removed excess comments; first draft of pp04 functions implemented. Needs testing.

--- Module02/Module02/main.cpp
+++ Module02/Module02/main.cpp
@@ -3,26 +3,11 @@
  * Provided by instructor.
  */
 
-/*
- // Returns the kth smallest value in anArray[first..last].
- kSmall(k: integer, anArray: ArrayType,first: integer, last: integer): ValueType
- {
- //Choose a pivot value p from anArray[first..last] p = 0
- //Partition the values of anArray[first..last] about p
- if (k < pivotIndex - first + 1)
- return kSmall(k, anArray, first, pivotIndex - 1)
- else if (k == pivotIndex - first + 1)
- return p
- else
- return kSmall(k - (pivotIndex - first + 1), anArray, pivotIndex + 1, last)
- }
- //This pseudocode is not far from a C++ function. The only questions that remain are how to choose the pivot value p and how to partition the array about the chosen p. The choice of p is arbitrary. Any p in the array will work, although the sequence of choices will affect how soon you reach the base case. Chapter 11 gives an algorithm for partitioning the values about p. There you will see how to turn the function kSmall into a sorting algorithm.
- */
-
 #include <iomanip>
 #include <iostream>
 #include <string>
 #include "pp04.cpp"
+
 using namespace std;
 int const ARRAY_SIZE = 6;
 
--- Module02/Module02/pp04.cpp
+++ Module02/Module02/pp04.cpp
@@ -1,12 +1,82 @@
 /*
- Name: Your Name
- Class: This Class Name
- Assignment: Name of this Assignment
+ Name: Daniel Bacon
+ Class: kSmall
+ Assignment: Module 2 - Programming Problem #4.
  */
-
+#include <algorithm>
 using namespace std;
 
-int kSmall(int k, int anArray[], int first, int last)
-{
-    return 0;
+
+void sortThreeEntries(int anArray[], int first, int middle, int last) {
+    if (anArray[first] > anArray[middle])
+        swap(anArray[first], anArray[middle]);
+    
+    if (anArray[middle] > anArray[last])
+        swap(anArray[middle], anArray[last]);
+
+    if (anArray[first] > anArray[middle])
+        swap(anArray[first], anArray[middle]);
+}
+
+
+
+
+
+
+int partition(int anArray[], int first, int last) {
+    int mid, pivot, pivotIndex, leftIndex, rightIndex;
+    bool finished = false;
+    
+    mid = first + (last - first) / 2;
+    sortThreeEntries(anArray, first, mid, last);
+    swap(anArray[mid], anArray[last - 1]);
+
+    pivotIndex = last - 1;
+    pivot = anArray[pivotIndex];
+    
+    leftIndex = first + 1;
+    rightIndex = last - 2;
+    
+    while (!finished) {
+        while (anArray[leftIndex] < pivot)
+            leftIndex += 1;
+        
+        while (anArray[rightIndex] > pivot)
+            rightIndex -= 1;
+        
+        if (leftIndex < rightIndex) {
+            //Move anArray[firstUnknown] into S1
+            swap(anArray[leftIndex], anArray[rightIndex]);
+            leftIndex += 1;
+            rightIndex -= 1;
+        } else {
+            finished = true;
+        }
+    }
+    
+    swap(anArray[pivotIndex], anArray[leftIndex]);
+    pivotIndex = leftIndex;
+    
+    
+    return pivotIndex;
+}
+
+
+
+
+
+
+int kSmall(int k, int anArray[], int first, int last) {
+    // Choose a pivot value p from anArray[first..last] p = 0
+    // Partition the values of anArray[first..last] about p
+    int pivotIndex = partition(anArray, first, last);
+
+    if (k < pivotIndex - first + 1)
+        return kSmall(k, anArray, first, pivotIndex - 1);
+
+    else if (k == pivotIndex - first + 1)
+        return pivotIndex;
+
+    else
+        return kSmall(k - (pivotIndex - first + 1), anArray, pivotIndex + 1, last);
 }
