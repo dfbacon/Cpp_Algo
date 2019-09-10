93c5070dcb7169c261bd5b0c07d51b2e7e4ed382
Daniel Bacon <dbacon338@gmail.com>
2019-09-10 16:35:09 -0700

    Simplified partition method. Working as expected.

--- Module02/Module02/main.cpp
+++ Module02/Module02/main.cpp
@@ -13,18 +13,21 @@ int const ARRAY_SIZE = 6;
 
 int main()
 {
-    int testArray[] = {6,4,1,2};
-    int postioin = 1;
-    cout << "TESTING\nanswer is: 1" << endl << "kSmall answer is: " << kSmall(postioin, testArray, 0, 3) << endl;
-//    int testArray1[] = {6,3,1,2,4,5}; //1, 2, 3, 4, 5, 6
-//    int position = 4;
-//    cout << "answer is: 4" << endl << "kSmall answer is: " << kSmall(position, testArray1, 0, ARRAY_SIZE -1) << endl;
-//    int testArray2[] = {20,11,12,-45,7,18}; //-45, 7, 11, 12, 18, 20
-//    position = 3;
-//    cout << "answer is: 11" << endl << "kSmall answer is: " << kSmall(position, testArray2, 0, ARRAY_SIZE -1) << endl;
-//    int testArray3[] = {1, 2, 3, 4, 5, 6};
-//    int position = 1;
-//    cout << "answer is: 1" << endl << "kSmall answer is: " << kSmall(position, testArray3, 0, ARRAY_SIZE - 1) << endl;
+    int testArray1[] = {6,3,1,2,4,5}; //1, 2, 3, 4, 5, 6
+    int position = 1;
+    cout << "answer is: 1" << endl << "kSmall answer is: " << kSmall(position, testArray1, 0, ARRAY_SIZE -1) << endl;
+    
+    int testArray2[] = {20,11,12,-45,7,18}; //-45, 7, 11, 12, 18, 20
+    position = 3;
+    cout << "answer is: 11" << endl << "kSmall answer is: " << kSmall(position, testArray2, 0, ARRAY_SIZE -1) << endl;
+    
+    int testArray[] = {4,3,1,2};
+    position = 2;
+    cout << "answer is: 2" << endl << "kSmall answer is: " << kSmall(position, testArray, 0, 3) << endl;
+    
+    int sortedArray[] = {1, 2, 3, 4, 5};
+    position = 3;
+    cout << "answer is: 3" << endl << "kSmall answer is: " << kSmall(position, sortedArray, 0, 4) << endl;
 
 }
 
--- Module02/Module02/pp04.cpp
+++ Module02/Module02/pp04.cpp
@@ -10,14 +10,18 @@ using namespace std;
 
 
 void sortThreeEntries(int anArray[], int first, int middle, int last) {
-    if (anArray[first] > anArray[middle])
-        swap(anArray[first], anArray[middle]);
-    
-    if (anArray[middle] > anArray[last])
-        swap(anArray[middle], anArray[last]);
 
-    if (anArray[first] > anArray[middle])
+    if (anArray[first] > anArray[middle]) {
         swap(anArray[first], anArray[middle]);
+    }
+    
+    if (anArray[middle] > anArray[last]) {
+        swap(anArray[middle], anArray[last]);
+    }
+    
+    if (anArray[first] > anArray[middle]) {
+        swap(anArray[first], anArray[middle]);
+    }
 }
 
 
@@ -26,42 +30,19 @@ void sortThreeEntries(int anArray[], int first, int middle, int last) {
 
 
 int partition(int anArray[], int first, int last) {
-    int mid, pivot, pivotIndex, leftIndex, rightIndex;
-    bool finished = false;
 
-
-    mid = first + (last - first) / 2;
-    sortThreeEntries(anArray, first, mid, last);
-    swap(anArray[mid], anArray[last - 1]);
-
-    pivotIndex = last - 1;
-    pivot = anArray[pivotIndex];
-
-    leftIndex = first + 1;
-    rightIndex = last - 2;
-
-    while (!finished) {
-
-        while (anArray[leftIndex] < pivot)
-            leftIndex += 1;
-
-        while (anArray[rightIndex] > pivot)
-            rightIndex -= 1;
-
-        if (leftIndex < rightIndex) {
-            //Move anArray[firstUnknown] into S1
-            swap(anArray[leftIndex], anArray[rightIndex]);
-            leftIndex += 1;
-            rightIndex -= 1;
-        } else {
-            finished = true;
+    int leftIndex = first;
+    int rightIndex = last;
+    for (int i = first; i < last; i++) {
+        if (anArray[i] < anArray[rightIndex]) {
+            swap(anArray[i], anArray[leftIndex]);
+            leftIndex++;
         }
     }
+    
+    swap(anArray[rightIndex], anArray[leftIndex]);
 
-    swap(anArray[pivotIndex], anArray[leftIndex]);
-    pivotIndex = leftIndex;
-
-    return pivotIndex;
+    return leftIndex;
 }
 
 
@@ -74,14 +55,15 @@ int kSmall(int k, int anArray[], int first, int last) {
     // Partition the values of anArray[first..last] about p
     int pivotIndex = partition(anArray, first, last);
     
-    if (k < pivotIndex - first + 1)
+    if (k < pivotIndex - first + 1) {
         return kSmall(k, anArray, first, pivotIndex - 1);
+    }
     
     else if (k == pivotIndex - first + 1) {
-        cout << "pivotIndex is " << pivotIndex << endl;
         return anArray[pivotIndex];
     }
     
-    else
+    else {
         return kSmall(k - (pivotIndex - first + 1), anArray, pivotIndex + 1, last);
+    }
 }
