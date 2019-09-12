cd5d5187abf63f4d833fdb98a3c99dc882964f86
Daniel Bacon <dbacon338@gmail.com>
2019-09-11 23:24:06 -0700

    Simplified partition, added edge conditions, working as expected.

--- Module02/Module02/pp04.cpp
+++ Module02/Module02/pp04.cpp
@@ -5,7 +5,6 @@
  */
 
 #include <algorithm>
-
 using namespace std;
 
 
@@ -29,29 +28,48 @@ void medianOfThree(int anArray[], int first, int middle, int last) {
 
 
 int partition(int anArray[], int first, int last) {
-    int pivot = anArray[first];
-    int leftIndex = first + 1;
-    int rightIndex = last - 2;
+    int mid, pivot, pivotIndex, leftIndex, rightIndex;
     bool finished = false;
     
+    if (first >= last) {
+        return first;
+    }
+    
+    mid = first + (last - first) / 2;
+    medianOfThree(anArray, first, mid, last);
+    swap(anArray[mid], anArray[last - 1]);
+    
+    pivotIndex = last - 1;
+    pivot = anArray[pivotIndex];
+    leftIndex = first + 1;
+    rightIndex = last - 2;
+    
     while (!finished) {
-        
-        while (anArray[leftIndex] < pivot) {
-            leftIndex++;
+        while (anArray[leftIndex] <= pivot) {
+            leftIndex += 1;
         }
         
         while (anArray[rightIndex] > pivot) {
-            rightIndex--;
+            rightIndex -= 1;
         }
         
-        if (leftIndex >= rightIndex) {
-            return rightIndex;
+        if (leftIndex < rightIndex) {
+            swap(anArray[leftIndex], anArray[rightIndex]);
+            leftIndex += 1;
+            rightIndex -= 1;
         }
-        
-        swap(anArray[leftIndex], anArray[rightIndex]);
+        else {
+            finished = true;
+        }
     }
     
-    return -1;
+    if (leftIndex < pivotIndex) {
+        swap(anArray[pivotIndex], anArray[leftIndex]);
+    }
+  
+    pivotIndex = leftIndex;
+    
+    return pivotIndex;
 }
 
 
@@ -60,22 +78,21 @@ int partition(int anArray[], int first, int last) {
 
 
 int kSmall(int k, int anArray[], int first, int last) {
-    int middleIndex = first + (last - first) / 2;
-    
-    medianOfThree(anArray, first, middleIndex, last);
-    swap(anArray[middleIndex], anArray[last - 1]);
+    if (k > last || k < 0 || first < 0 || first > last || last < 0) {
+        return -1;
+    }
     
     int pivotIndex = partition(anArray, first, last);
     
-    if (k < pivotIndex - first + 1) {
+    if (k < pivotIndex) {
         return kSmall(k, anArray, first, pivotIndex - 1);
     }
     
-    else if (k == pivotIndex - first + 1) {
-        return anArray[pivotIndex];
+    else if (k == pivotIndex) {
+        return anArray[pivotIndex - 1];
     }
     
     else {
-        return kSmall(k - (pivotIndex - first + 1), anArray, pivotIndex + 1, last);
+        return kSmall(k, anArray, pivotIndex + 1, last);
     }
 }
