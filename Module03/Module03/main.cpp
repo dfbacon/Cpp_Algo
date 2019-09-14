80613ea302347cb9169f787ebd920d2ed2a76ee0
Daniel Bacon <dbacon338@gmail.com>
2019-09-14 13:22:35 -0700

    Creating new module.

---
+++ Module03/Module03/main.cpp
@@ -0,0 +1,37 @@
+/*
+ Name: Daniel Bacon
+ Class: kSmall
+ Assignment: Module 3 - Programming Problem #1.
+ */
+
+#include <iostream>
+
+/*
+ Programming Problem #1
+ Data Abstraction and Problem Solving with C++ 6th ed page 115
+ Design and implement an ADT that represents the time of day. Represent the time as hours and minutes on a 24-hour clock. The hours and minutes are the private data members of the class that implements the ADT. Include at least two initialization operations: one that provides a default value for the time, and another that sets the time to a client-supplied value. These operations are the class’s constructors. Also include operations that set the time, increase the present time by a number of minutes, and display the time in 12-hour and 24-hour notations.
+ 
+ Demonstrate your ADT in a main function.
+ 
+ Student Learning Objectives.
+ This is our first foray into developing an ADT. This has the advantage of being a simple ADT. This clearly relates to the SLO #3 "Design and implement data structures and algorithms, including array-based and linked list implementations," even though this solution does not require an array or linked list.
+ Submission and Assessment
+ To submit this assignment, use the submit button and submit (Links to an external site.) your .cpp file.
+ You can expect to get a your assignment grade and feedback in one (1) week.
+ Your assessment will be based on the assignment rubric (The rubric can be found at the bottom of the assignment page).
+ Key topics  give you an idea of what are important aspects of the assignment. They are also used in conjunction with the grading rubric for assessing your grade . The best way to get a good grade is to make sure your assignment addresses all the key topics.
+ Key Topics
+ Input
+ The ADT includes interfaces for set the time and increase the present time by a number of minutes.
+ Users cannot impact non-interface members of the ADT.
+ Processing
+ The ADT handles various time related math (e.g. increasing time by 60 minutes increases the number of hours displayed by 1).
+ Correct use of initialization and constructors.
+ Output
+ The ADT correctly handles different time displays ("display the time in 12-hour and 24-hour notations.")
+ */
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
