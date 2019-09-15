c4b88b18967f4ec460216c161d7b12ff3e3dfd2c
Daniel Bacon <dbacon338@gmail.com>
2019-09-15 12:10:07 -0700

    Added interface with documentation. Added class basics to implement interface, with basic testing.

--- Module03/Module03/main.cpp
+++ Module03/Module03/main.cpp
@@ -1,37 +1,150 @@
 /*
  Name: Daniel Bacon
- Class: kSmall
+ Class: Clock
  Assignment: Module 3 - Programming Problem #1.
  */
 
 #include <iostream>
-
+#include <iomanip>
+#include <string>
+using namespace std;
 /*
  Programming Problem #1
- Data Abstraction and Problem Solving with C++ 6th ed page 115
- Design and implement an ADT that represents the time of day. Represent the time as hours and minutes on a 24-hour clock. The hours and minutes are the private data members of the class that implements the ADT. Include at least two initialization operations: one that provides a default value for the time, and another that sets the time to a client-supplied value. These operations are the class’s constructors. Also include operations that set the time, increase the present time by a number of minutes, and display the time in 12-hour and 24-hour notations.
  
- Demonstrate your ADT in a main function.
+ Design and implement an ADT that represents the time of day. Represent the time as hours and minutes on a 24-hour clock. The hours and minutes are the private data members of the class that implements the ADT.
  
- Student Learning Objectives.
- This is our first foray into developing an ADT. This has the advantage of being a simple ADT. This clearly relates to the SLO #3 "Design and implement data structures and algorithms, including array-based and linked list implementations," even though this solution does not require an array or linked list.
- Submission and Assessment
- To submit this assignment, use the submit button and submit (Links to an external site.) your .cpp file.
- You can expect to get a your assignment grade and feedback in one (1) week.
- Your assessment will be based on the assignment rubric (The rubric can be found at the bottom of the assignment page).
- Key topics  give you an idea of what are important aspects of the assignment. They are also used in conjunction with the grading rubric for assessing your grade . The best way to get a good grade is to make sure your assignment addresses all the key topics.
+     Also include operations that set the time, increase the present time by a number of minutes, and display the time in 12-hour and 24-hour notations.
+ 
  Key Topics
- Input
+ 
  The ADT includes interfaces for set the time and increase the present time by a number of minutes.
  Users cannot impact non-interface members of the ADT.
- Processing
+ 
  The ADT handles various time related math (e.g. increasing time by 60 minutes increases the number of hours displayed by 1).
  Correct use of initialization and constructors.
- Output
+ 
  The ADT correctly handles different time displays ("display the time in 12-hour and 24-hour notations.")
  */
+
+class ClockInterface {
+
+public:
+    /** Displays the current time in the format HH:MM. */
+    virtual void displayTime() const = 0;
+    
+    /** Sets the time.
+     @post The time has been set to the new hour and minute values.
+     @param hours The new hour value.
+     @param minutes The new minute value. */
+    virtual void setTime(const int hours, const int minutes) = 0;
+    
+    /** Retrieves the current minute value. */
+    virtual void getMinutes() const = 0;
+    
+    /** Adds a given number of minutes to the time.
+     @post The time has been increased by the given number of minutes.
+     @param minutesToAdd The number of minutes to add.
+     @return The advanced minute value. */
+    virtual int addMinutes(const int minutesToAdd) = 0;
+    
+    /** Retrieves the current hour value. */
+    virtual void getHour() const = 0;
+    
+    /** Increases the hour value by a given amount.
+     @post The time has been increased by the given number of hours.
+     @param hoursToAdd The number of hours to add.
+     @return The advanced hour value. */
+    virtual int increaseHour(const int hoursToAdd) = 0;
+};
+
+
+
+
+
+
+class Clock : public ClockInterface {
+
+private:
+    
+    static const int MAX_MINUTES = 60;
+    static const int MAX_HOUR_12 = 12;
+    static const int MAX_HOUR_24 = 24;
+    int hours, minutes;
+    bool is24;
+    
+    /** Sets the clock to 12- or 24-hour mode.
+     @return Client decision on clock mode. */
+    bool set24() {
+        string userResponse;
+        
+        cout << "DEFAULT NOTATION: 12 hour.\nWould you like to set clock to " <<
+        "24 hour mode? (Y/N): ";
+        
+        cin >> userResponse;
+        if (userResponse == "y" || userResponse == "Y") {
+            return true;
+        }
+        
+        return false;
+    }
+    
+public:
+    
+    Clock(int hours = 00, int minutes = 00) : hours(hours), minutes(minutes) {
+        is24 = set24();
+    }
+    
+    void displayTime() const {
+        cout << std::setfill('0') << std::setw(2) << hours << ":" <<
+        std::setfill('0') << std::setw(2) << minutes << endl;
+    }
+    
+    
+    
+    
+    
+    
+    void setTime(const int hours, const int minutes) {}
+    
+    
+    
+    
+    
+    
+    void getMinutes() const {}
+    
+    
+    
+    
+    
+    
+    int addMinutes(const int minutesToAdd) { return 0; }
+    
+    
+    
+    
+    
+    
+    void getHour() const {}
+    
+    
+    
+    
+    
+    
+    int increaseHour(const int hoursToAdd) { return 0; }
+};
+
+
+
+
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Clock noArgClock;
+    Clock argClock(4, 30);
+    
+    noArgClock.displayTime();
+    argClock.displayTime();
     return 0;
 }
