dcc1e96809ef69e9dc94b905dcbb5cb021bf086b
Daniel Bacon <dbacon338@gmail.com>
2019-09-24 16:19:05 -0700

    Initialization and setup for new assignment.

---
+++ Module04/Module04/main.cpp
@@ -0,0 +1,44 @@
+//
+//  main.cpp
+//  Module04 - Programming Problem #6
+//
+//  Created by Daniel Bacon on 9/24/19.
+//  Copyright © 2019 Daniel Bacon. All rights reserved.
+//
+
+/*
+ Define a class DoublyLinkedBag that implements the ADT bag by using a doubly
+ linked chain, as shown in Figure 4-9. Use the class of nodes that Exercise
+ 10 defines.
+ 
+ Demonstrate your double linked list implementation in a main function.
+ 
+ Tips: Unlike the previous assignment (with arrays) we are not using integers
+ as our data - but the ADT bag, which uses templates.
+ 
+ It would be best to implement the ADT linked based implementation of bag first.
+ This majority of this code is done for you in the textbook. Then simply modify
+ the LinkedBag code to handle a double linked list.
+ 
+ Key Topics
+    Modification of the Node class: Constructor/descructor for the new node (w/
+        a next and prev pointers)
+ 
+    Modification of the Node class: addition of setPrev and getPrev Functions
+
+    Modification of the LinkedBag class: constructors and destructors
+ 
+    Modification of the LinkedBag class: add and remove
+
+    main.cpp demonstrates the initialization and destruction of a double linked
+        list.
+ 
+    main.cpp  demonstrates an addition and removal from the double linked list.
+ */
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
