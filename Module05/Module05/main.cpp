f146850de991ba20ee5e93d5f56f3d2551543fa1
Daniel Bacon <dbacon338@gmail.com>
2019-09-30 16:57:06 -0700

    Added working solution.

--- Module05/Module05/main.cpp
+++ Module05/Module05/main.cpp
@@ -4,18 +4,73 @@
 // Assignment: Module 05 - Programming Problem 02
 //
 
-/**
- Consider the language that the following grammar defines:
- 
- <S> =  $ | <W> | $<S>
- 
- <W> = abb|a<W>bb
- 
- Write all strings that are in this language and that contain seven or fewer characters.
- */
 
 #include <iostream>
+using namespace std;
+
+
+/**
+    Checks a given string for validity.
+    Valid strings must follow these requirements:
+        <S> =  $ | <W> | $<S>
+        <W> = abb|a<W>bb
+ 
+    @param str String being evaluated.
+    @return true if string is valid, false otherwise.
+ */
+bool validPattern(string str);
+
+
+/**
+    Prints valid strings to the console.
+ 
+    @param str String being evaluated.
+ */
+void writeString(string str);
+
+
+
+
+
+
+bool validPattern(string str) {
+    if (str.length() > 0 && str.length() <= 7) {
+        if (str[0] == '$') {
+            return true;
+        }
+        else if (str[0] == 'a' && str[str.length() - 1] == 'b' &&
+                 str[str.length() - 2] == 'b') {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+
+
+
+void writeString(string str) {
+    if (validPattern(str)) {
+        cout << str << endl;
+    }
+}
+
+
+
+
+
 
 int main(int argc, const char * argv[]) {
+    
+    string testArray[] = {"$", "$I'm too long", "invalid", "$valid", "abb",
+        "a$12bb", "a$bbb", "a$b", "a$invalidbb", "ayesbb", "atoolongbb", ""};
+    //  true, false, false, true, true, true, true, false, false, true, false, false
+    
+    for (int index = 0; index < 12; index++) {
+        writeString(testArray[index]);
+    }
+    
     return 0;
 }
