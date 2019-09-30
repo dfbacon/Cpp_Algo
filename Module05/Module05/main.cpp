a860f1ada5929cb69a8e0c9c15e535bc33b56b41
Daniel Bacon <dbacon338@gmail.com>
2019-09-30 10:14:59 -0700

    New assignment setup.

---
+++ Module05/Module05/main.cpp
@@ -0,0 +1,21 @@
+//
+// Name: Daniel Bacon
+// Class:
+// Assignment: Module 05 - Programming Problem 02
+//
+
+/**
+ Consider the language that the following grammar defines:
+ 
+ <S> =  $ | <W> | $<S>
+ 
+ <W> = abb|a<W>bb
+ 
+ Write all strings that are in this language and that contain seven or fewer characters.
+ */
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
