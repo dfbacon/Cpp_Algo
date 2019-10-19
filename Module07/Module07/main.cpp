7593a740940a48a16902a88b2be9869282fedd8e
Daniel Bacon <dbacon338@gmail.com>
2019-10-19 10:14:21 -0700

    Updated testing for out of bounds indexes.

--- Module07/Module07/main.cpp
+++ Module07/Module07/main.cpp
@@ -1,38 +1,9 @@
 //
-//  main.cpp
-//  Module07
+//  Name: Daniel Bacon
+//  Assignment: Module07 - Programming Problems 2 and 3
 //
-//  Created by Daniel Bacon on 10/13/19.
-//  Copyright © 2019 Daniel Bacon. All rights reserved.
 //
 
-/*
- Programming Problem #2 and 3
- Data Abstraction and Problem Solving with C++ 6th ed page 287
-    Write an array-based implementation of the ADT list that expands the size of
-    the array of list entries as needed so that the list can always accommodate a
-    new entry.
- 
- Also reduce the size of the array as needed to accommodate several removals.
-    When the size of the array is greater than 20 and the number of entries in
-    the list is less than half the size of the array, reduce the size of the
-    array so that it is three quarters of its current size.
- 
- Note: In expanding the array - when an element is added to a full array, double
-    the size of the array.
- 
- Key Topics
- Input
-    List detects and responds to a need for expansion
-    List detects and responds to a need for contraction
- Processing
-    Correct implementation of the array based list as described in the textbook.
-    Use of templates
-    Use of interface
- Output
-    main function demonstrates lists features and dynamic sizing
-    Proper clean up - no dangling pointers, no memory leaks.
- */
 #include <iostream>
 #include <cassert>
 using namespace std;
@@ -301,6 +272,14 @@ int main(int argc, const char * argv[]) {
             cout << "Removed first entry. List size should be 23 : " <<
                 intList.getLength() << endl;
         }
+        
+        intList.insert(1000, 777);
+        cout << "Inserting new element outside of array bounds. List size " <<
+            "should still be 23 : " << intList.getLength() << endl;
+        
+        intList.remove(1000);
+        cout << "Removing outside of array bounds. List size " <<
+            "should still be 23 : " << intList.getLength() << endl;
     
         cout << "Removing multiple items..." << endl;
         for (int i = 24; i > 12; i--) {
